431e4554add8f7bc159b458f32a6f1f1a192f813
Lebohang Sopazi <72252063+DaLebza10111@users.noreply.github.com>
2024-01-12 15:25:36 +0200

    Register the Pizza service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using BlazingPizza.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+// Register the pizzas service
+builder.Services.AddSingleton<PizzaService>();
 
 if (!app.Environment.IsDevelopment())
 {
